41c41d6e6c9dfdafa4e42ca774319deb4de3c2df
dtbuild <dtbuild@rigil>
2026-09-11 12:08:31 +0000

    13ee95dd46967bade46b507b4de58f84c0891af6 Release 2.0.0
    Sync to source repo @13ee95dd46967bade46b507b4de58f84c0891af6

--- js/stateRestore.bootstrap5.mjs
+++ js/stateRestore.bootstrap5.mjs
@@ -1,4 +1,4 @@
-/*! StateRestore Bootstrap 5 styling 2.0.0-dev for DataTables
+/*! StateRestore Bootstrap 5 styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
